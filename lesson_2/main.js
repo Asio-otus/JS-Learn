1b96e26e412e717368a73cd841417c5d4ffccb28
Mark Kadiner <dasistmail765@gmail.com>
2020-12-18 00:01:16 +0300

    all the question from the page on the link in lesson 2 are done

--- lesson_2/main.js
+++ lesson_2/main.js
@@ -50,3 +50,42 @@ for (let i = 0; i < 10; i++) {
 }
 console.log(arr4);
 
+// 8 //
+let randomArr = [-5, 55, 5, 10, 6, 7, -2, 11, 8, -9];
+let newArr = [];
+for (let i = 0; i < randomArr.length; i++) {
+    if (randomArr[i] < 10 && randomArr[i] > 0) {
+        newArr.push(randomArr[i]);
+    }
+}
+console.log(newArr);
+
+// 9 //
+for (let i = 0; i < randomArr.length; i++) {
+    if (randomArr[i] === 5) {
+        console.log('There is number 5 in the array')
+        break;
+    }
+}
+
+// 10 //
+let sum = 0;
+for (let i = 0; i < randomArr.length; i ++) {
+    sum += randomArr[i]
+}
+console.log(sum);
+
+// 11 //
+let sum2 = 0;
+for (let i = 0; i < randomArr.length; i ++) {
+    sum2 += randomArr[i] * randomArr[i];
+}
+console.log(sum2);
+
+// 12 //
+let sum3 = 0;
+for (let i = 0; i < randomArr.length; i ++) {
+    sum3 += randomArr[i];
+}
+console.log(sum3 / randomArr.length);
+
